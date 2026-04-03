f5cadfec0e281f40af084948a65271915c6fa8c6
ajlevy246 <ajlevy246@gmail.com>
2026-04-03 00:35:18 -0400

    cleanup

--- fine-tuning-attacks/CHUD_finetune.py
+++ fine-tuning-attacks/CHUD_finetune.py
@@ -6,18 +6,11 @@ from transformers import (
     AutoModelForCausalLM,
     AutoTokenizer,
     BitsAndBytesConfig,
-    Trainer,
     TrainingArguments
 )
 from trl import SFTTrainer
 import torch
 import argparse
-import pandas as pd
-from tqdm import tqdm
-import gc
-import os
-import re
-import time
 from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
 from datasets import load_dataset
 
